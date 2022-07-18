723a280dd2bfd70ad719c7a1d1d9649f3a1f75e3
nvarshney91 <varshneynishant0@gmail.com>
2022-07-19 00:27:31 +0530

    Displaying the guesses

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
-export default function Row () {
+export default function Row ({guess}) {
+  if(guess){
+    return(
+        <div className='text-center flex justify-center'>
+            {guess.map((l,i)=>(
+                <div key={i} className='block w-[60px] h-[60px] border-solid border-[1px] border-[#bbb] m-[4px] text-center leading-[60px] uppercase font-bold text-[2.5em]'>
+                   {l.key}
+                </div>
+            ))}
+        </div>
+    )
+  }
   return (
     <div className='text-center flex justify-center'>
          <div className='block w-[60px] h-[60px] border-solid border-[1px] border-[#bbb] m-[4px] text-center leading-[60px] uppercase font-bold text-[2.5em]'></div>
@@ -8,7 +19,6 @@ export default function Row () {
          <div className='block w-[60px] h-[60px] border-solid border-[1px] border-[#bbb] m-[4px] text-center leading-[60px] uppercase font-bold text-[2.5em]'></div>
          <div className='block w-[60px] h-[60px] border-solid border-[1px] border-[#bbb] m-[4px] text-center leading-[60px] uppercase font-bold text-[2.5em]'></div>
          <div className='block w-[60px] h-[60px] border-solid border-[1px] border-[#bbb] m-[4px] text-center leading-[60px] uppercase font-bold text-[2.5em]'></div>
-    </div>
-    
+    </div>    
   )
 }
